3ab5a1352ef42c1dc14a28e453bb7d66266d2ef3
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-21 22:39:10 +0300

    Update 2.LastKNumbersSequence.js

--- 2.LastKNumbersSequence.js
+++ 2.LastKNumbersSequence.js
@@ -2,12 +2,10 @@ function lastKNumbersSequence(n, k) {
     // n - length new arr
     // k - sum of previous k elements
     let newArr = [1];
-
     for (let i = 1; i < n; i++) {
         let start = Math.max(0, i - k);
         let curEl = newArr.slice(start, start + k).reduce((a, b) => a + b, 0);
         newArr.push(curEl);
-
     }
     return newArr
 }
